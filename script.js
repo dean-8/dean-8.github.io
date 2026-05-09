ca874864dce51f6ffb8918cf7314da0473c9935e
dean-8 <dean.potishman@gmail.com>
2026-05-09 21:25:36 +0100

    js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // auto update copyright year
 document.getElementById('year').textContent = new Date().getFullYear(); 
+
+function dropdown_menu(id){
+  let aa = document.getElementById(id);
+  if (aa.style.display === "none" || aa.style.display === ""){
+    aa.style.display = "block";
+  }
+  else{
+    aa.style.display = "none";
+  }
+  return false;
+}
